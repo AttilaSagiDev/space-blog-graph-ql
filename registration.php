aabf7e58c2d2fd0a88da22c010d1a42c3334232b
Attila Sági <attila.sagi@itegration.com>
2023-08-25 11:31:57 +0200

    Add base module files

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2023, Open Software License ("OSL") v. 3.0
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Space_BlogGraphQl', __DIR__);
